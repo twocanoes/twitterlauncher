835d0b94c7b04b426b5aba0365c0e3ec2d1d6616
Dave Lebbing <dlebbing@twocanoes.com>
2013-05-01 15:19:07 -0500

    Added selection list to choose which app to launch.  Tweetbot will now launch into url path if applicable.

--- Twitter Launcher.safariextension/twitter.js
+++ Twitter Launcher.safariextension/twitter.js
@@ -1,11 +1,12 @@
 // from http://stackoverflow.com/questions/3026686/safari-extension-questions
 function getMessage(msgEvent) {
 
-    window.location=msgEvent.message
+    window.location=msgEvent.message + window.location.pathname.substring(1);
 
 }
 
-safari.self.tab.dispatchMessage("getSettingValue", "urlHandler"); // ask for value
+//safari.self.tab.dispatchMessage("getSettingValue", "urlHandler"); // ask for value
+safari.self.tab.dispatchMessage("getSettingValue", "chooseHandler"); // ask for value
 safari.self.addEventListener("message", getMessage, false); // wait for reply
 
 

632281130b9a79c4cdeb164c8290e526a4399731
tperfitt <tperfitt@twocanoes.com>
2013-05-01 00:02:36 -0500

    added preference to add handler.

--- Twitter Launcher.safariextension/twitter.js
+++ Twitter Launcher.safariextension/twitter.js
@@ -1,1 +1,12 @@
-if(window.location.href.match(/^https:\/\/twitter.com.*/)){window.location='twitter://'}
+// from http://stackoverflow.com/questions/3026686/safari-extension-questions
+function getMessage(msgEvent) {
+
+    if (msgEvent.name == "settingValueIs")
+	if(window.location.href.match(/^https:\/\/twitter.com.*/)){window.location=msgEvent.message}
+
+}
+
+safari.self.tab.dispatchMessage("getSettingValue", "urlHandler"); // ask for value
+safari.self.addEventListener("message", getMessage, false); // wait for reply
+
+
